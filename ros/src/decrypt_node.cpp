ed3323b2512199afbb2eea00259b83d516cc53d6
heymosbrother <danielshencool@gmail.com>
2022-09-05 17:22:50 +0800

    pack things into class to make pub&sub easier

--- ros/src/decrypt_node.cpp
+++ ros/src/decrypt_node.cpp
@@ -1,83 +1,102 @@
 #include "ros/ros.h"
 #include "std_msgs/Int8.h"
+#include "std_msgs/String.h"
 
 #include <math.h>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-/**
- * A global array to store the received number and a global index for that array
- */
-int counter = 0;
-int dataArr[440];
-
-/**
- * @brief Decrypt the message stored in the global array and then print on the terminal
- */
-void decryptMsg()
+class SubscribeAndPublish
 {
+private:
+    ros::NodeHandle nh;
+    ros::Publisher pub;
+    ros::Subscriber sub;
+    int dataArr[440];
+    int counter = 0;
     string originalMsg = "";
 
-    int b = ceil(sqrt(440));
-    int a = floor(sqrt(440));
-
-    if (a * b < 440)
-        a++;
-
-    // Create and fill the Matrix with Encrypted message
-    char matrix[a][b];
-
-    int index = 0; 
-    for (int j = 0; j < b; j++)
+public:
+    SubscribeAndPublish()
     {
-        for (int i = 0; i < a; i++)
+        pub = nh.advertise<std_msgs::String>("decrypted_message", 1);
+        sub = nh.subscribe("encrypted_message", 100, &SubscribeAndPublish::receiveMsg, this);
+        for (int i = 0; i < 440; i++)
+            dataArr[i] = 0;
+    }
+
+    /**
+     * @brief Get the message from bag file and store them into the array member
+     * @param msg - the received ASCII number
+     */
+    void receiveMsg(const std_msgs::Int8::ConstPtr &msg)
+    {
+        dataArr[counter] = (int)(msg->data);
+        // Print the received message on terminal
+        // ROS_INFO("[%d]\tReceived, #%d,\ttransform to [%c]", msg->data, counter + 1, dataArr[counter] - 3);
+        counter++;
+        if (counter >= 440)
+            decryptMsg();
+    }
+
+    /**
+     * @brief Decrypt the message stored in the global array and then print on the terminal
+     */
+    void decryptMsg()
+    {
+
+        int b = ceil(sqrt(440));
+        int a = floor(sqrt(440));
+
+        if (a * b < 440)
+            a++;
+
+        // Create and fill the Matrix with Encrypted message
+        char matrix[a][b];
+
+        int index = 0;
+        for (int j = 0; j < b; j++)
         {
-            if (index < 440)
-            {    
-                matrix[j][i] = dataArr[index] - 3;
+            for (int i = 0; i < a; i++)
+            {
+                if (index < 440)
+                {
+                    matrix[j][i] = dataArr[index] - 3;
+                }
+                index++;
             }
-            index++;
         }
-    }
 
-    // Read and store the decrypted message
-    for (int j = 0; j < a; j++)
-    {
-        for (int i = 0; i < b; i++)
+        // Read and store the decrypted message
+        for (int j = 0; j < a; j++)
         {
-            originalMsg = originalMsg + matrix[i][j];
+            for (int i = 0; i < b; i++)
+            {
+                originalMsg = originalMsg + matrix[i][j];
+            }
         }
+
+        // Print out the decrypted message on the terminal
+        ROS_INFO("a = %d, b = %d, Decrypted Message: \n%s", a, b, originalMsg.c_str());
+
+        std_msgs::String decryptedResult;
+        stringstream ss;
+        ss << originalMsg;
+        decryptedResult.data = ss.str();
+
+        pub.publish(decryptedResult);
     }
-
-    // Print out the decrypted message on the terminal
-    ROS_INFO("a = %d, b = %d, Decrypted Message: \n%s", a, b, originalMsg.c_str());
-}
-
-/**
- * @brief Get the message from bag file and store them into the global array
- * @param msg - the received ASCII number
- */
-void receiveMsg(const std_msgs::Int8::ConstPtr &msg)
-{
-    dataArr[counter] = (int)(msg->data);
-    // Print the received message on terminal
-    // ROS_INFO("[%d]\tReceived, #%d,\ttransform to [%c]", msg->data, counter + 1, dataArr[counter] - 3);
-    counter++;
-    if (counter >= 440)
-        decryptMsg();
-}
+};
 
 int main(int argc, char **argv)
 {
-    for(int i = 0; i < 440; i++)
-    {
-        dataArr[i] = 0;
-    }
-
+    // Initialize the current node
     ros::init(argc, argv, "decryped_node");
-    ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe("encrypted_message", 1000, receiveMsg);
+
+    SubscribeAndPublish SubPubObject;
+
     ros::spin();
 
     return 0;
